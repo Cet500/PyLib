c19fb294ec21cde3fe07d5ebf9382377dbfee618
Cet <korlyakov.2001@mail.ru>
2020-11-02 00:22:16 +1000

    add PySiteTest

---
+++ PySiteTest/cgi-bin/index.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+print("Content-type: text/html")
+print("")
+
+print("""
+	<h1>Hello world!</h1>
+	<h2>I create this with Python</h2>
+""")
+
+for i in range(1,10):
+	print("I Love Python<br>")
